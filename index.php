bfb3de64ab935060fb9c5dda5855fec580fe70d5
HasibRahmanSourov <rhasib36@gmail.com>
2020-08-23 13:25:36 -0700

    project

---
+++ index.php
@@ -0,0 +1,154 @@
+<?php 
+
+	//include header file
+	include ('include/header.php');
+
+?>
+
+
+<div class="container-fluid header-img">
+				<div class="row">
+					<div class="col-md-6 offset-md-3">
+
+						<div class="header">
+							<h1 class="text-center" style="color: red;"><marquee> Donate the blood, save the life</marquee> </h1>
+						<h3 class="text-center">Donate the blood to help the others.</h3>
+						</div>
+
+
+						<h1 class="text-center">Search The Donors</h1>
+						<hr class="white-bar text-center">
+
+						<form action="search.php" method="get" class="form-inline text-center" style="padding: 40px 0px 0px 5px;">
+							<div class="form-group text-center justify-content-center">
+							
+					<select style="width: 220px; height: 45px;" name="city" id="city" class="form-control demo-default" required>
+							<option value="">Select Your Area</option>
+							<optgroup title="Dhaka (Bangladesh)" label="&raquo; Dhaka (Bangladesh)"></optgroup>
+							<option value="Adabor" >Adabor</option>
+							<option value="Uttar Khan" >Uttar Khan</option>
+							<option value="Uttara" >Uttara</option>
+							<option value="Kadamtali" >Kadamtali</option>
+							<option value="Kalabagan" >Kalabagan</option>
+							<option value="Kafrul" >Kafrul</option>
+							<option value="Kamrangirchar" >Kamrangirchar</option>
+							<option value="Cantonment" >Cantonment</option>
+							<option value="Kotwali" >Kotwali</option>
+							<option value="Khilkhet" >Khilkhet</option>
+							<option value="Khilgaon" >Khilgaon</option>
+							<option value="Gulshan" >Gulshan</option>
+							<option value="Chawkbazar Model" >Chawkbazar Model</option>
+							<option value="Demra" >Demra</option>
+							<option value="Gendaria" >Gendaria</option>
+							<option value="Turag" >Turag</option>
+							<option value="Tejgaon" >Tejgaon</option>
+							<option value="Tejgaon I/A" >Tejgaon I/A</option>
+							<option value="Dakshinkhan" >Dakshinkhan</option>
+							<option value="Darus Salam" >Darus Salam</option>
+							<option value="Dhanmondi" >Dhanmondi</option>
+							<option value="New Market" >New Market</option>
+							<option value="Paltan" >Paltan</option>
+							<option value="Pallabi" >Pallabi</option>
+							<option value="Bangshal" >Bangshal</option>
+							<option value="Badda" >Badda</option>
+							<option value="Bimanbandar" >Bimanbandar</option>
+							<option value="Motijheel" >Motijheel</option>
+							<option value="Mirpur Model" >Mirpur Model</option>
+							<option value="Mohammadpur" >Mohammadpur</option>
+							<option value="Jatrabari" >Jatrabari</option>
+							<option value="Ramna" >Ramna</option>
+							<option value="Rampura" >Rampura</option>
+							<option value="Lalbagh" >Lalbagh</option>
+							<option value="Shah Ali" >Shah Ali</option>
+							<option value="Shahbagh" >Shahbagh</option>
+							<option value="Sher-e-Bangla Nagar" >Sher-e-Bangla Nagar</option>
+							<option value="Shyampur" >Shyampur</option>
+							<option value="Sabujbagh" >Sabujbagh</option>
+							<option value="Sutrapur" >Sutrapur</option>
+							<option value="Hazaribagh" >Hazaribagh</option>
+							<option value="Wari" >Wari</option>
+				</select>
+							</div>
+							<div class="form-group center-aligned">
+								<select name="blood_group" id="blood_group" style="padding: 0 20px; width: 220px; height: 45px;" class="form-control demo-default text-center margin10px">
+
+									<option value="">Select Your Blood Group</option>
+									<option value="A+">A+</option>
+									<option value="A-">A-</option>
+									<option value="B+">B+</option>
+									<option value="B-">B-</option>
+									<option value="AB+">AB+</option>
+									<option value="AB-">AB-</option>
+									<option value="O+">O+</option>
+									<option value="O-">O-</option>
+
+								</select>
+							</div>
+							<div class="form-group center-aligned">
+								<button type="submit" class="btn btn-lg btn-danger">Search</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+			<!-- header ends -->
+
+			<!-- donate section -->
+			<div class="container-fluid red-background">
+				<div class="row">
+					<div class="col-md-12">
+						<h1 class="text-center"  style="color: white; font-weight: 700;padding: 10px 0 0 0;">Donate The Blood</h1>
+						<hr class="white-bar">
+						<p class="text-center pera-text">
+							We are a group of exceptional programmers; our aim is to promote education. If you are a student, then contact us to secure your future. We deliver free international standard video lectures and content. We are also providing services in Web & Software Development.
+
+							We are a group of exceptional programmers; our aim is to promote education. If you are a student, then contact us to secure your future. We deliver free international standard video lectures and content. We are also providing services in Web & Software Development.
+						</p>
+						<a href="#" class="btn btn-default btn-lg text-center center-aligned">Become a Life Saver!</a>
+					</div>
+				</div>
+			</div>
+			<!-- end doante section -->
+			
+
+			<div class="container">
+				<div class="row">
+    				<div class="col">
+    					<div class="card">
+     						<h3 class="text-center red">Our Vission</h3>
+								<img src="img/binoculars.png" alt="Our Vission" class="img img-responsive" width="168" height="168">
+								<p class="text-center">
+									We are a group of exceptional programmers; our aim is to promote education. If you are a student, then contact us to secure your future. We deliver free international standard video lectures and content. We are also providing services in Web & Software Development.
+								</p>
+						</div>
+    				</div>
+    				
+    				<div class="col">
+    					<div class="card">
+      							<h3 class="text-center red">Our Goal</h3>
+								<img src="img/target.png" alt="Our Vission" class="img img-responsive" width="168" height="168">
+								<p class="text-center">
+									We are a group of exceptional programmers; our aim is to promote education. If you are a student, then contact us to secure your future. We deliver free international standard video lectures and content. We are also providing services in Web & Software Development.
+								</p>
+						</div>
+    				</div>
+    			
+    				<div class="col">
+    					<div class="card">
+      						<h3 class="text-center red">Our Mission</h3>
+								<img src="img/goal.png" alt="Our Vission" class="img img-responsive" width="168" height="168">
+								<p class="text-center">
+									We are a group of exceptional programmers; our aim is to promote education. If you are a student, then contact us to secure your future. We deliver free international standard video lectures and content. We are also providing services in Web & Software Development.
+								</p>
+							</div>
+   			 		</div>
+ 			</div>
+ 		</div>
+
+			<!-- end aboutus section -->
+
+
+<?php
+//include footer file
+include ('include/footer.php');
+ ?>
